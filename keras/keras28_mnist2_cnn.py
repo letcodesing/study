d6da3701cf8bce889c0a9295c9803a354ebedef5
letcodesing <103926252+letcodesing@users.noreply.github.com>
2022-07-08 19:23:17 +0900

    argmax

--- keras/keras28_mnist2_cnn.py
+++ keras/keras28_mnist2_cnn.py
@@ -1,16 +1,22 @@
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 from tensorflow.keras.datasets import mnist
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, StandardScaler, RobustScaler
 #1.데이터
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
 x_train = x_train.reshape(60000,28,28,1)
 x_test = x_test.reshape(10000,28,28,1)
 print(x_train.shape)
 import numpy as np
 print(np.unique(y_train,return_counts=True))
 
+
 from tensorflow.keras.utils import to_categorical
 y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+print(y_train[:5])
 #acc 0.98이상
 #cnn 3개이상
 
@@ -18,6 +24,7 @@ y_train = to_categorical(y_train)
 model = Sequential()
 model.add(Conv2D(filters=64, kernel_size=(3,3), padding='same', input_shape=(28,28,1))) 
 model.add(MaxPooling2D())
+model.add(Conv2D(7, (2,2), padding='valid', activation='relu'))
 model.add(Conv2D(7, (2,2), padding='valid', activation='relu'))
 model.add(Conv2D(7, (2,2), padding='valid', activation='relu'))
 model.add(Flatten())
@@ -31,11 +38,18 @@ model.compile(loss='categorical_crossentropy', optimizer='adam')
 model.fit(x_train, y_train, epochs=10, batch_size=20)
 #발리 콜백 생략
 
+print(y_test)
 #4.평가 훈련
 loss = model.evaluate(x_test, y_test)
 y_predict = model.predict(x_test)
-
+print(y_test)
+print(y_predict)
+y_predict = np.argmax(y_predict, axis= 1)
+y_test = np.argmax(y_test, axis= 1)
 
 from sklearn.metrics import accuracy_score
 acc = accuracy_score(y_test, y_predict)
 print(acc)
+
+# 0.9809
+# 
