5dbfe1605a4431601931b28b3da8c30cfa7ba76d
letcodesing <103926252+letcodesing@users.noreply.github.com>
2022-07-16 23:32:21 +0900

    Update keras45_lstm_kaggle_jena2.py

--- keras/keras45_lstm_kaggle_jena2.py
+++ keras/keras45_lstm_kaggle_jena2.py
@@ -1,17 +1,33 @@
 #split lstm conv1d rnn cnn 시계열 데이터
 import pandas as pd
 import numpy as np
+import time
 data = pd.read_csv('c:/study/_data/kaggle_jena/jena_climate_2009_2016.csv')
 data = data.transpose()
 print(data[:5])
-print(data.shape)
+print(data.shape) #(15, 420551)
 def split(seq, size): 
     aaa = []
-    for i in range(len(seq) - size + 1):
-        subset = seq[i:(i+size)]
+    for i in range(420551 - 574):
+        subset = np.reshape(seq.iloc[:,i*size-1:575+size*i],(seq.iloc[:,i*size-1:575+size*i],1))
         aaa.append(subset)
     print(type(aaa))
-    return np.array(aaa)
-
-data = data.iloc[:,:143]
-print(data)
+    return aaa
+size = 144
+s_time = time.time()
+b = split(data,size)
+print(b)
+print(time.time()-s_time)
+print(b.shape)
+data2 = data.iloc[:,-1:575]
+print(data2)
+data3 = data.iloc[:,0:719]
+print(data3)
+data4 = data.iloc[:,420550:]
+print(data4)
+# data5 = data.iloc[:,431:575]
+# print(data5)
+# def mix(a):
+#     subset = a.iloc[:,:100]
+#     return print(subset.shape)
+# b = mix(data)
